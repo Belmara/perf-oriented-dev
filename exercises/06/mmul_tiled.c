c8571134ced89250c77f11efb24a2c0242411d25
Maximilian Maier <csaz8293@student.uibk.ac.at>
2024-05-07 23:15:39 +0200

    zip file for thoman

---
+++ exercises/06/mmul_tiled.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define S 2048
+#define N S
+#define M S
+#define K S
+
+#define BLOCK_SIZE 8
+
+#define MIN(X,Y) ((X)<(Y)?(X):(Y))
+#define MAX(X,Y) ((X)>(Y)?(X):(Y))
+
+#define TYPE double
+#define MATRIX TYPE**
+
+// A utility function
+MATRIX createMatrix(unsigned x, unsigned y) {
+	TYPE* data = malloc(x * y * sizeof(TYPE));
+
+	TYPE** index = malloc(x * sizeof(TYPE*));
+	index[0] = data;
+	for (unsigned i = 1; i < x; ++i) {
+		index[i] = &(data[i*y]);
+	}
+	return index;
+}
+
+void freeMatrix(MATRIX matrix) {
+	free(matrix[0]);
+	free(matrix);
+}
+
+
+int main(void) {
+
+	// create the matrices
+	MATRIX A = createMatrix(N, M);
+	MATRIX B = createMatrix(M, K);
+	MATRIX C = createMatrix(N, K);
+
+	// initialize the matrices
+
+	// A contains real values
+	for (int i=0; i<N; i++) {
+		for (int j=0; j<M; j++) {
+			A[i][j] = i*j;
+		}
+	}
+
+	// B is the identity matrix
+	for (int i=0; i<M; i++) {
+		for (int j=0; j<K; j++) {
+			B[i][j] = (i==j)?1:0;
+		}
+	}
+
+	// conduct multiplication
+    for (int block_i = 0; block_i < S; block_i += BLOCK_SIZE){
+        for (int block_j = 0; block_j < S; block_j += BLOCK_SIZE){
+            for (int block_k =0; block_k < S; block_k += BLOCK_SIZE){
+
+                for (int i=block_i; i<MIN(block_i + BLOCK_SIZE, S); ++i) {
+					
+                    for (int j=block_j; j<MIN(block_j + BLOCK_SIZE, S); ++j) {
+                        TYPE sum = 0;
+                        for (int k=block_k; k<MIN(block_k + BLOCK_SIZE, S); ++k) {
+                            sum += A[i][k] * B[k][j];
+                        }
+                        C[i][j] += sum;
+                    }
+                }
+            }
+        }
+    }
+
+	// verify result
+	int success = 1;	
+	for (int i=0; i<N; i++) {
+		for (int j=0; j<MIN(M,K); j++) {
+			if (A[i][j] != C[i][j]) {
+				success = 0;
+			}
+		}
+		for (int j=MIN(M,K); j<MAX(M,K); j++) {
+			if (C[i][j] != 0) {
+				success = 0;
+			}
+		}
+	}
+
+	// print verification result
+	printf("Verification: %s\n", (success)?"OK":"ERR");
+
+	freeMatrix(A);
+	freeMatrix(B);
+	freeMatrix(C);
+
+	return success ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
